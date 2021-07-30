ecd4169f6a4503e8b88e13351a55c342de7fbde0
Yash Charpe <yashrcharpe07@gmail.com>
2021-07-30 21:10:01 +0530

    Splash Screen Added

--- app/src/main/java/com/example/covid19trackerapp/MainActivity.java
+++ app/src/main/java/com/example/covid19trackerapp/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.covid19trackerapp;
 
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,8 +15,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+        
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
